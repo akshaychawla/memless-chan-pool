5d2dbacdaf9c38a6f0fcc33b84633c5c075dd29d
akshay <Akshay Chawla>
2019-12-16 15:56:21 -0500

    PRCNPTN and FastPRCNPTN
    New version of PRCN paper.
    No need for repetition.
    Modified GradientTest.py and PRCN.py for the changes based on the paper.
    Running GradientTest.py shows that they work equally.

--- GradientTest.py
+++ GradientTest.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn 
 import torch 
 import random 
-from PRCN import PRCNv1_noconv, PRCNv2_noconv, PRCNv1, PRCNv2 
+from PRCN import PRCNv1_noconv, PRCNv2_noconv, PRCNv1, PRCNv2, PRCNPTN, FastPRCNPTN
 from copy import deepcopy 
 import cupy as cp
 
@@ -11,50 +11,55 @@ import cupy as cp
 ip_chans, op_chans = 8,16 
 h,w = 128, 128 
 batch_size = 16 
-G = 4 
-exp = 3
+G = 12 
+# exp = 3
+CMP = 4
 print("Performing forward and backward pass checks:") 
-print("Parameters: ip_chans: {} | op_chans: {} | batch_size: {} | G: {} | exp: {}".format(
-        ip_chans, op_chans, batch_size, G, exp))
+# print("Parameters: ip_chans: {} | op_chans: {} | batch_size: {} | G: {} | exp: {}".format(
+#         ip_chans, op_chans, batch_size, G, exp))
+print("Parameters: ip_chans: {} | op_chans: {} | batch_size: {} | G: {} | CMP: {}".format(
+        ip_chans, op_chans, batch_size, G, CMP))
 
 # Pre-requisites for shape, randomlist
 ltest_ip = torch.randn(batch_size, ip_chans, h, w).float().cuda()
-ltest = PRCNv1(ip_chans,op_chans,G=G,exp=exp,kernel_size=3,padding=1,stride=1).cuda()
+ltest = PRCNPTN(ip_chans,op_chans,G=G,CMP=CMP,kernel_size=3,padding=1,stride=1).cuda()
 ltest.eval()
 ltest_op, ltest_convop = ltest.forward(ltest_ip)
-print("PRCN op shape: {} , PRCN Conv layer op shape: {}".format(ltest_op.shape, ltest_convop.shape))
+print("PRCNPTN op shape: {} , PRCNPTN Conv layer op shape: {}".format(ltest_op.shape, ltest_convop.shape))
 
 # data from conv layer 
-x_conv = ltest_convop.detach()
-x_conv.requires_grad = True
-x_conv.retain_grad() 
-randomList = np.array(ltest.randomList)
+ltest_ip = ltest_ip.detach()
+ltest_ip.requires_grad = True
+ltest_ip.retain_grad() 
+randomList = deepcopy(ltest.randomList)
 
-# GT to regress against 
+# # GT to regress against 
 gt = torch.rand_like(ltest_op.detach().cpu()).cuda()
 mse = nn.MSELoss()
 
-# no-conv Layers 
-layer1 = PRCNv1_noconv(ip_chans, op_chans, G=G, exp=exp, kernel_size=3, padding=1, stride=1, randomList=randomList).cuda() 
-layer2 = PRCNv2_noconv(ip_chans, op_chans, G=G, exp=exp, kernel_size=3, padding=1, stride=1, randomList=randomList).cuda() 
+# # no-conv Layers 
+layer1 = PRCNPTN(ip_chans, op_chans, G=G, CMP=CMP, kernel_size=3, padding=1, stride=1, randomList=randomList).cuda()
+layer2 = FastPRCNPTN(ip_chans, op_chans, G=G, CMP=CMP, kernel_size=3, padding=1, stride=1, randomList=randomList).cuda()
+# Copy weights 
+layer2.conv1.load_state_dict(layer1.conv1.state_dict())
 
-# Test backward pass prcn v1 
-op1 = layer1(x_conv) 
+# Test backward pass PRCNPTN 
+op1,_ = layer1(ltest_ip) 
 op1.retain_grad()
 loss = mse(op1, gt) 
 loss.backward()
-grad1 = x_conv.grad.cpu().detach()
+grad1 = ltest_ip.grad.cpu().detach()
 op1_grad = op1.grad.cpu().detach().numpy()
 op1_val   = op1.cpu().detach().numpy()
 del loss
-x_conv.grad.zero_()
+ltest_ip.grad.zero_()
 
-# Test backward pass prcn v2
-op2 = layer2(x_conv) 
+# # Test backward pass FastPRCNPTN 
+op2,_ = layer2(ltest_ip) 
 op2.retain_grad()
 loss = mse(op2, gt) 
 loss.backward()
-grad2 = x_conv.grad.cpu().detach()
+grad2 = ltest_ip.grad.cpu().detach()
 op2_grad = op2.grad.cpu().detach().numpy()
 op2_val   = op2.cpu().detach().numpy()
 del loss 
